bf5f0312f4db0bcf3d92563dff8b366a4d1bb302
yeomsungpil <present7256@gmail.com>
2023-11-13 11:37:37 +0900

    List / Section / 프로퍼티 추가 / 다양한 modifier 개념 및 custom modifier를 사용하여 코드 개선

--- SeSac3SwiftUI/MenuView.swift
+++ SeSac3SwiftUI/MenuView.swift
@@ -8,15 +8,45 @@
 import SwiftUI
 
 struct MenuView: View {
-    var body: some View {
-        HStack(spacing: 15) {
-            CardView()
-            CardView()
-            CardView()
-            
+    
+    // 다른 View에서 사용하지 않으면 프로퍼티로 만들어서 사용할 수 있음
+    var cardProperty: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Image(systemName: "star")
+                .background(.red)
+            Text("cardProperty")
+                .foregroundColor(.white)
         }
-       
+        .padding()
+        .background(.blue)
+    }
+    
+    var body: some View {
+        VStack {
+            HStack(spacing: 15) {
+                CardView(text: "1312")
+                cardProperty
+                CardView(text: "Gfdg")
+                 
+            }
             
+            List {
+                Section("첫번째 섹션") {
+                    DatePicker("Date", selection: .constant(Date()))
+                    Text("보안과 인증")
+                        .modifier(PointBoardText())
+                    Text("내 신용점수")
+                    Text("보안과 인증")
+                }
+                
+                Section("자산") {
+                    Text("보안과 인증")
+                    Text("내 신용점수")
+                    Text("보안과 인증")
+                }
+            }
+            .listStyle(.plain)
+        }
     }
 }
 
@@ -29,14 +59,36 @@ struct MenuView_Previews: PreviewProvider {
 }
 
 struct CardView: View {
+    
+    let text : String
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             Image(systemName: "star")
                 .background(.red)
-            Text("토스 월드")
+            Text(text)
                 .foregroundColor(.white)
+                // 밑 줄
+                .underline(color: .red)
+                // 취소선
+                .strikethrough()
         }
         .padding()
         .background(.blue)
     }
+}
+
+// 반복되는 modifier를 커스텀으로 만들어보기
+struct PointBoardText: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .padding(10)
+            .foregroundColor(.white)
+            .background(.purple)
+            .clipShape(Capsule())
+    }
+    
+    
 }
