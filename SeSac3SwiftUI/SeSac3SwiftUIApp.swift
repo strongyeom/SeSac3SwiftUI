6ed262245e55b4274261bdf7a6e5e242554c3df1
yeomsungpil <present7256@gmail.com>
2023-11-13 09:42:13 +0900

    Initial Commit

---
+++ SeSac3SwiftUI/SeSac3SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSac3SwiftUIApp.swift
+//  SeSac3SwiftUI
+//
+//  Created by 염성필 on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct SeSac3SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
