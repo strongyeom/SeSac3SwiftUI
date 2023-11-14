6ff7c205838abd4edd17b9cb63a1e8d44624b796
yeomsungpil <present7256@gmail.com>
2023-11-14 11:32:45 +0900

    @State 깊게 이해하기

--- SeSac3SwiftUI/TamagochiView.swift
+++ SeSac3SwiftUI/TamagochiView.swift
@@ -25,14 +25,17 @@ struct User {
     }
 }
 
+// ⭐️ UI는 데이터에 의존해서 자신의 상태를 결정하게 된다.
+// @State == SOT (Source of Truth) , 상태 프로퍼티(State Property)
 struct TamagochiView: View {
     
-   @State var nickname: String = "고래고래"
+   @State private var nickname: String = "고래고래" // View에 대한 상태를 관리하기 위해 존재
+    
     // body 연산 프로퍼티 안에 get {} 이 있지만 생략해서 사용하는 것이고, nickname을 바꿀려면 get 앞에 mutating을 작성해야한디.
     // ⭐️ SwiftUI에서는 body에 get만 사용할 수 있고 mutating을 사용할 수 없음 -> 변수를 수정 할 수 없음 -> @State 사용
     var body: some View {
         VStack {
-            Text("Hello, World! \(nickname)")
+            Text("Hello, World! \(nickname)") // 데이터가 달라질때 View를 다시 Reendering 함
                 .background(.black)
                 .foregroundColor(.white)
                 .font(.title)
