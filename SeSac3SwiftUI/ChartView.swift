9ee1b852271d4893a3f0f2efd958104a9f7e0ce7
yeomsungpil <present7256@gmail.com>
2023-11-17 11:04:39 +0900

    @ViewBuilder 개념 및 활용방법

--- SeSac3SwiftUI/ChartView.swift
+++ SeSac3SwiftUI/ChartView.swift
@@ -10,9 +10,26 @@ import Charts
 
 struct ChartView: View {
     let movie : [Movie]
+    
+    
+    @Environment(\.colorScheme) var color
+    
+    //  @ViewBuilder : 구조체로 따로 빼지 않고 한개의 구조체에서 프로퍼티를 만들어 UI에 표현할까 사용
+    // 즉, body를 제외한 모든 프로퍼티가 UI에 적용되려면 사용해야함 => 어느정도 생략해도 괜찮지만 if 문으로 분기처리 되었을때는 사용해야한다.
+    @ViewBuilder
+    var chartTitle: some View {
+        if color == .dark {
+            Text("다크 차트")
+        } else {
+            Text("라이트 차트")
+        }
+    }
+    
+    // body : 내부에 @ViewBuilder 내장되어 있음
     var body: some View {
+        
         VStack {
-            Text("차트")
+           chartTitle
             // data: 보여주고 싶은 모델
             // id : UUID 값 or 구조체 자체가 Hashable하다면 \.self 사용 가능
             Chart(movie, id: \.self) { item in
